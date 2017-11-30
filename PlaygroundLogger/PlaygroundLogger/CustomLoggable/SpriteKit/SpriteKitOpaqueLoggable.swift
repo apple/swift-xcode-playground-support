62d0cbf158f5a043d5a5c6b540e13f221bf246d3
Connor Wakamo <cwakamo@apple.com>
2017-11-29 21:20:25 -0500

    Implemented `CustomOpaqueLoggable` for the SpriteKit types.

---
+++ PlaygroundLogger/PlaygroundLogger/CustomLoggable/SpriteKit/SpriteKitOpaqueLoggable.swift
@@ -0,0 +1,42 @@
+//===--- SpriteKitOpaqueLoggable.swift ------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2017 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+import SpriteKit
+
+@objc fileprivate class SpriteKitCopyImageDataHook: NSObject {
+    @objc(_copyImageData) func _copyImageData() -> Data? {
+        fatalError()
+    }
+}
+
+fileprivate protocol SpriteKitOpaqueLoggable: class, OpaqueImageRepresentable, CustomOpaqueLoggable {}
+
+extension SpriteKitOpaqueLoggable {
+    func encodeImage(into encoder: LogEncoder, withFormat format: LogEncoder.Format) {
+        guard let copyImageDataMethod = (self as AnyObject)._copyImageData, let imageData = copyImageDataMethod() else {
+            fatalError("Unable to get image data, unable to encode anything")
+        }
+        
+        _ = imageData
+        
+        fatalError("SpriteKit image encoding not yet implemented")
+    }
+    
+    var opaqueRepresentation: LogEntry.OpaqueRepresentation {
+        return ImageOpaqueRepresentation(kind: .sprite, backedBy: self)
+    }
+}
+
+extension SKShapeNode: SpriteKitOpaqueLoggable {}
+extension SKSpriteNode: SpriteKitOpaqueLoggable {}
+extension SKTextureAtlas: SpriteKitOpaqueLoggable {}
+extension SKTexture: SpriteKitOpaqueLoggable {}
