16f18164ce0e103a494bbb65dae2e4b379d8e7d2
Connor Wakamo <cwakamo@apple.com>
2017-12-11 16:32:21 -0800

    Added an empty umbrella header to the PlaygroundSupport framework.
    SourceKit currently is only able to provide code completion in `import` statements for Clang modules.
    As a result, to ensure that PlaygroundSupport shows up in code completion, include an empty umbrella header.
    This causes PlaygroundSupport.framework to provide a Clang module in addition to the Swift module.
    
    As part of this commit, also fixed a handful of build settings in the PlaygroundSupport_* targets.

---
+++ PlaygroundSupport/PlaygroundSupport/PlaygroundSupport.h
@@ -0,0 +1,17 @@
+//===--- PlaygroundSupport.h ----------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+//
+// This header is intentionally left blank.
+// It exists for the aid of code completion, which currently can only complete
+// Clang modules.
+//
+//===----------------------------------------------------------------------===//
