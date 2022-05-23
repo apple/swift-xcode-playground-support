6b3cb114403587e25493ed148bee29172f066b14
Connor Wakamo <cwakamo@apple.com>
2022-05-23 15:41:27 -0700

    Fixed a warning in XCPlaygroundPage.swift.

--- PlaygroundSupport/XCPlayground/XCPlaygroundPage.swift
+++ PlaygroundSupport/XCPlayground/XCPlaygroundPage.swift
@@ -44,9 +44,7 @@ public final class XCPlaygroundPage {
     ///
     /// - note: This function has been deprecated. 
     @available(*,deprecated) public func captureValue<T>(value: T, withIdentifier identifier: String) {
-        if let value = value as? AnyObject {
-            NotificationCenter.default.post(name: "XCPCaptureValue" as NSString as NSNotification.Name, object: self, userInfo: [ "value" : value, "identifier": identifier as AnyObject])
-        }
+        NotificationCenter.default.post(name: "XCPCaptureValue" as NSString as NSNotification.Name, object: self, userInfo: [ "value" : value, "identifier": identifier as AnyObject])
     }
 
     /// Indicates whether the playground page needs to execute indefinitely.
